3b68d5c507693ec430d84d7136785b25f7ddfd15
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-01-24 13:43:51 +0200

    ez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/Home.component';
+import { AboutComponent } from './About/About.component';
+import { ContactComponent } from './Contact/Contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HomeComponent,
+      AboutComponent,
+      ContactComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
