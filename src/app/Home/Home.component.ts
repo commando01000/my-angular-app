995957f591a7c8ab16900b41a61661926bbec54b
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-04 14:52:18 +0200

    Update Home.component.ts

--- src/app/Home/Home.component.ts
+++ src/app/Home/Home.component.ts
@@ -9,7 +9,7 @@ import { RecipesService } from '../Recipes.service';
   styleUrls: ['./Home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  products: Product[] = [];
+  // products: Product[] = [];
   recipes: any[] = [];
   // constructor() {
   //   let myService = new UsersService();
@@ -29,37 +29,39 @@ export class HomeComponent implements OnInit {
       console.log(this.recipes);
     });
   }
-  ngOnInit() {}
+  ngOnInit() {
 
-  // userName: string = 'Supraa';
-  // userAge: number = 25;
-  // onSale:boolean = false;
-  // price:number = 3000;
-  // link:string = 'https://google.com';
-  // imgURL:string = 'https://picsum.photos/200';
+  }
 
-  // products: Product[] = [
-  //   {
-  //     productName: 'HP',
-  //     productPrice: 20000,
-  //     productDescription: 'laptop',
-  //     productImage: 'https://picsum.photos/200',
-  //   },
-  //   {
-  //     productName: 'Dell',
-  //     productPrice: 30000,
-  //     productDescription: 'laptop',
-  //     productImage: 'https://picsum.photos/200',
-  //   },
-  //   {
-  //     productName: 'Mac',
-  //     productPrice: 90000,
-  //     productDescription: 'laptop',
-  //     productImage: 'https://picsum.photos/200',
-  //   }
-  // ]
+  userName: string = 'Supraa';
+  userAge: number = 25;
+  onSale:boolean = false;
+  price:number = 3000;
+  link:string = 'https://google.com';
+  imgURL:string = 'https://picsum.photos/200';
 
-  // welcome() {
-  //   alert('welcome');
-  // }
+  products: Product[] = [
+    {
+      productName: 'HP',
+      productPrice: 20000,
+      productDescription: 'laptop',
+      productImage: 'https://picsum.photos/200',
+    },
+    {
+      productName: 'Dell',
+      productPrice: 30000,
+      productDescription: 'laptop',
+      productImage: 'https://picsum.photos/200',
+    },
+    {
+      productName: 'Mac',
+      productPrice: 90000,
+      productDescription: 'laptop',
+      productImage: 'https://picsum.photos/200',
+    }
+  ]
+
+  welcome() {
+    alert('welcome');
+  }
 }
